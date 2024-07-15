5f2439eb6842b7ef7ad6b356c0526320f39724ad
hun9008 <younghune135@gmail.com>
2024-07-15 16:20:02 +0900

    model server docker run

--- routes/route.py
+++ routes/route.py
@@ -12,6 +12,8 @@ from models.inference_ViT import vit_inference
 # from models.inference_ViT import inference
 import matplotlib.pyplot as plt
 from PIL import Image
+import json
+import numpy as np
 
 router = APIRouter()
 
@@ -30,7 +32,18 @@ async def inference(img: dict):
     
     output = vit_inference(encoding_img)
 
-    # print(output)
+    def numpy_converter(obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        raise TypeError("Object of type '%s' is not JSON serializable" % type(obj).__name__)
 
-    return output
+    json_str = json.dumps(output, default=numpy_converter) 
+
+    print("output : ",output)
+
+    print("output type : ", type(output))
+
+    print("json_str : ", json_str)
+
+    return json_str
     
