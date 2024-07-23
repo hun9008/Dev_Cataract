b0f3891809333d5630cd659ffb7302b2ad3943cc
gae-ddong <emma030209@ajou.ac.kr>
2024-07-23 16:00:53 +0900

    FEAT : Add img to base64 & POST Request

--- Screen/NewCam/App.js
+++ Screen/NewCam/App.js
@@ -1,18 +1,20 @@
-import { CameraView, useCameraPermissions} from 'expo-camera';
-import { useState } from 'react';
+import { Camera, CameraType } from 'expo-camera/legacy';
+import { useState, useRef } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
-  const [facing, setFacing] = useState('back');
-  const [permission, requestPermission] = useCameraPermissions();
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const cameraRef = useRef(null); // Create a ref for the camera
+  const [photo, setPhoto] = useState(null); // State to store the photo
 
   if (!permission) {
-    // Camera permissions are still loading.
+    // Camera permissions are still loading
     return <View />;
   }
 
   if (!permission.granted) {
-    // Camera permissions are not granted yet.
+    // Camera permissions are not granted yet
     return (
       <View style={styles.container}>
         <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
@@ -21,21 +23,48 @@ export default function App() {
     );
   }
 
-  function toggleCameraFacing() {
-    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
   }
 
+  // 사진 촬영
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const options = { quality: 1, base64: true, exif: false };
+      const photo = await cameraRef.current.takePictureAsync(options);
+      setPhoto(photo);
 
+      let dataToSend = { img : photo.base64 };
+      console.log(dataToSend);
+
+      fetch('http://cataractmodel.hunian.site/inference', {
+        method: 'POST',
+        body: JSON.stringify(dataToSend),
+        headers: {
+          // Header Definition
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((response) => response.json())
+        .then((responseJson) => {
+          console.log(responseJson);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
+      <Camera style={styles.camera} type={type} ref={cameraRef}>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
             <Text style={styles.text}>Flip Camera</Text>
           </TouchableOpacity>
         </View>
-      </CameraView>
+      </Camera>
+      <Button title="사진 촬영" onPress={takePicture} />
     </View>
   );
 }
