04f7eb97db0be09883cb8f8f1ea7e9be43323a7a
gae-ddong <emma030209@ajou.ac.kr>
2024-07-26 15:12:16 +0900

    FEAT : Merge Cam & Login

--- App.js
+++ App.js
@@ -5,13 +5,15 @@ import SelectGalleryOrCam from './Screen/SelectGalleryOrCam';
 import Cam from './Screen/Cam';
 import Gallery from './Screen/Gallery';
 import CameraResult from './Screen/CameraResult';
+import Login from './Screen/Login';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SelectGalleryOrCam">
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="SelectGalleryOrCam" component={SelectGalleryOrCam} />
         <Stack.Screen name="Cam" component={Cam} />
         <Stack.Screen name="Gallery" component={Gallery} />
